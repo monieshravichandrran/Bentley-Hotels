c9eed86ee69b1d2c1c3cf96c2c2f068c5127ac7d
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-19 13:12:06 +0530

    Create menu.py

---
+++ menu.py
@@ -0,0 +1,10 @@
+#importing necessary python libraries
+from tkinter import*
+from tkinter import messagebox
+from PIL import ImageTk
+
+class Menu:
+    def __init__(self,root):
+        self.root=root
+        self.root.title("Bentley Hotels-Menu")
+        
