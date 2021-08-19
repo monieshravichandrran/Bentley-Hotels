8e28dd2edb0ba7eb7798e99afa17f32e4d625c94
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-19 14:09:57 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -4,7 +4,9 @@ from tkinter import messagebox
 from PIL import ImageTk
 
 class Menu:
-    def __init__(self,root):
-        self.root=root
-        self.root.title("Bentley Hotels-Menu")
+    def __init__(self,Frame2):
+        self.Frame2=Frame2
+        self.bg = ImageTk.PhotoImage(file="images/logo.png")
+        self.bg_img = Label(self.Frame2, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
+
         
