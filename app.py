48545af06f788c07e9ac872e6dd4bd0d7d8c8512
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-21 00:31:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
-#importing necessary python libraries
 import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import ImageTk
 from login import Login
 from menu import Menu
 
-window=tk.Tk()
-window.title("Bentley Hotels")
-window.iconbitmap("images/logo.png")
-window.geometry("1219x800+100+0")
-#window.resizable(False, False)
-window.rowconfigure(0,weight=1)
-window.columnconfigure(0,weight=1)
-Frame1=tk.Frame(window)
-obj1=Login(Frame1)
-if obj1.islog:
-    Frame2=tk.Frame(window)
-    Frame2.tkraise()
-    obj2=Menu(Frame2)
-window.mainloop()
+class App(tk.Tk):
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        container = tk.Frame(self)
+        self.title("Bentley Hotels")
+        self.iconbitmap("images/logo.png")
+        self.geometry("1219x800+100+0")
+        container.pack(side="top", fill="both", expand=True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (Login, Menu):
+            frame = F(container, self)
+            self.frames[F] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame(Login)
+
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+
+app = App()
+app.mainloop()
