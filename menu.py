02564405a7a9e1e8ac7995c5735d72b83a909946
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-21 00:34:26 +0530

    Update menu.py

--- menu.py
+++ menu.py
@@ -1,12 +1,18 @@
-#importing necessary python libraries
-from tkinter import*
-from tkinter import messagebox
+import tkinter as tk
 from PIL import ImageTk
 
-class Menu:
-    def __init__(self,Frame2):
-        self.Frame2=Frame2
-        self.bg = ImageTk.PhotoImage(file="images/logo.png")
-        self.bg_img = Label(self.Frame2, image=self.bg).place(x=0, y=0, relwidth=1, relheight=1)
-
-        
+class Menu(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.grid(row=0, column=0, stick="nsew")
+        self.bg = ImageTk.PhotoImage(file="images/im2.jpeg")
+        self.bg_img = tk.Label(self, image=self.bg)
+        self.bg_img.place(x=0, y=0, relwidth=1, relheight=1)
+        cust=MenuBars(self,controller)
+        cust.place(x=150,y=50,height=300,width=350)
+        emp=MenuBars(self,controller)
+        emp.place(x=550,y=50,height=300,width=350)
+        Bill=MenuBars(self,controller)
+        Bill.place(x=150,y=400,height=300,width=350)
+        logout=MenuBars(self,controller)
+        logout.place(x=550,y=400,height=300,width=350)
