a99ac9ef5134d59acbb37704b8bd517fc35d5d52
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-23 00:06:06 +0530

    Create hotel.py

---
+++ hotel.py
@@ -0,0 +1,12 @@
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk
+from menu import Menu
+
+class Hotel(tk.Frame):
+    def __int__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        self.grid(row=0, column=0, stick="nsew")
+        self.bg = ImageTk.PhotoImage(file="images/im2.jpeg")
+        self.bg_img = tk.Label(self, image=self.bg)
+        self.bg_img.place(x=0, y=0, relwidth=1, relheight=1)
