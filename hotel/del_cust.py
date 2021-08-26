f0bd5056cb00c97fed427c08107cd0f65e644b50
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-26 22:22:27 +0530

    Create del_cust.py

---
+++ hotel/del_cust.py
@@ -0,0 +1,11 @@
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk
+
+class Del_cust(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        self.grid(row=0, column=0, stick="nsew")
+        self.bg = ImageTk.PhotoImage(file="images/im2.jpeg")
+        self.bg_img = tk.Label(self, image=self.bg)
+        self.bg_img.place(x=0, y=0, relwidth=1, relheight=1)
