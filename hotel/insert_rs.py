ef63407de9adad2115681d843826ff8c7d32a6f5
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-29 23:41:39 +0530

    Add files via upload

---
+++ hotel/insert_rs.py
@@ -0,0 +1,20 @@
+from tkinter import *
+import tkinter as tk
+from PIL import ImageTk
+import sqlite3
+
+class Ins_rs(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self, parent)
+        self.grid(row=0, column=0, stick="nsew")
+        self.bg = ImageTk.PhotoImage(file="images/im2.jpeg")
+        self.bg_img = tk.Label(self, image=self.bg)
+        self.bg_img.place(x=0, y=0, relwidth=1, relheight=1)
+        ins = Ins_rs_Fram(self, controller)
+        ins.place(x=250, y=150, height=500, width=600)
+
+class Ins_rs_Fram(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent, bg="#FFFDD0", highlightbackground="black", highlightthickness=5)
+        disp = tk.Label(parent, text="INSERTION", bg="#FFFDD0", fg="#F99B03", font=("Helventica", 35, "bold"))
+        disp.place(x=400, y=170)
