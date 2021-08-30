93c9cb65fa4be46be711a0cf75e4ea16ced6dbad
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-31 02:34:49 +0530

    Update app.py

--- hotel/app.py
+++ hotel/app.py
@@ -31,11 +31,12 @@ from insert_rs import Ins_rs
 from update_rs import Upd_rs
 from delete_rs import Del_rs
 from package_management import Pack
+from restaurantbill import Restaurant_Bill
 #Dictionary to keep track of the Frames with a identifier as their key
 
 frame_name={"Lg":Login,"M":Menu,"C":Customer,"B":Bill,"H":Hotel,
             "CU":Upd_cust,"CI":Ins_cust,"CD":Del_cust,"S":Staff,"R":Restaurant,
-            "SI":Ins_staff,"SU":Upd_staff,"SD":Del_staff,"RI":Ins_rs,"RU":Upd_rs,"RD":Del_rs,"P":Pack}
+            "SI":Ins_staff,"SU":Upd_staff,"SD":Del_staff,"RI":Ins_rs,"RU":Upd_rs,"RD":Del_rs,"P":Pack,"FB":Restaurant_Bill}
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
@@ -51,7 +52,7 @@ class App(tk.Tk):
         container.grid_columnconfigure(0, weight=1)
         self.frames = {}
         self.frame_list=[Login,Menu,Customer,Bill,Hotel,Ins_cust,Upd_cust,Del_cust,Staff,Restaurant,
-                         Ins_staff,Upd_staff,Del_staff,Ins_rs,Upd_rs,Del_rs,Pack]
+                         Ins_staff,Upd_staff,Del_staff,Ins_rs,Upd_rs,Del_rs,Pack,Restaurant_Bill]
         for F in (self.frame_list):
             frame = F(container, self)
             self.frames[F] = frame
