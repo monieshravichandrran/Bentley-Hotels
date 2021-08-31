490d35c01efc792214f92dfb696970dac50765fa
monieshravichandrran <89132516+monieshravichandrran@users.noreply.github.com>
2021-08-31 13:36:43 +0530

    Update restaurantbill.py

--- hotel/restaurantbill.py
+++ hotel/restaurantbill.py
@@ -1,7 +1,8 @@
 from tkinter import *
 import tkinter as tk
 from PIL import ImageTk
-
+import sqlite3
+total_price=0
 class Restaurant_Bill(tk.Frame):
     def __init__(self,parent,controller):
         tk.Frame.__init__(self,parent)
@@ -17,8 +18,32 @@ class Restaurant_Bill(tk.Frame):
         self.resbill.place(x=250,y=150,height=500,width=600)
 
 class Restaurant_Bill_Fram(tk.Frame):
+    def onBackClick(self,parent,controller):
+        parent.reset(controller)
+        controller.show_frame("B")
     def onClickCalc(self,parent,controller,name,qty):
-        pass
+        global total_price
+        self.calculate['state']=DISABLED
+        conn=sqlite3.connect('bentley.db')
+        try:
+            c=conn.cursor()
+            c.execute('''
+                select * from restaurant where name=?;
+            ''',(name,))
+            l=c.fetchall()
+            price=l[0][3]
+            total_price=float(price)*int(qty)
+            self.eu = tk.Label(parent, text="COMPUTED!!\nCLICK BACK TO ADD MORE TO CART", font=("Arial", 10, "bold"), bg="#FFFDD0",
+                          fg="black")
+            self.eu.place(x=300, y=550)
+            print(total_price)
+        except:
+            self.eu = tk.Label(parent, text="NO SUCH ITEM EXISTS!!!\nCLICK BACK TO CONTINUE BILL CALCULATION", font=("Arial", 10, "bold"), bg="#FFFDD0",
+                          fg="black")
+            self.eu.place(x=300, y=550)
+            total_price=0
+        conn.commit()
+        conn.close()
     def __init__(self,parent,controller):
         tk.Frame.__init__(self, parent, bg="#FFFDD0", highlightbackground="black", highlightthickness=5)
         tk.Label(parent,text="FOOD BILLING", bg="#FFFDD0",fg="#F99B03",font=("Helventica",35,"bold")).place(x=400,y=170)
@@ -30,6 +55,8 @@ class Restaurant_Bill_Fram(tk.Frame):
         qty_d.place(x=330,y=380)
         self.qty_d=tk.Entry(parent,font=("Arial",15),bg="#FFFDD0",fg="black")
         self.qty_d.place(x=460,y=385)
-        self.calculate=tk.Button(parent,text="CALCULATE",fg="white",width=7,height=1,bg="green",font=("Helventica", 15, "bold"),
+        self.calculate=tk.Button(parent,text="COMPUTE",fg="white",width=10,height=1,bg="green",font=("Helventica", 15, "bold"),
                                  command=lambda: self.onClickCalc(parent,controller,self.name_d.get(),self.qty_d.get()))
         self.calculate.place(x=300,y=450)
+        back=tk.Button(parent,text="BACK",fg="white",bg="red",command=lambda: self.onBackClick(parent,controller))
+        back.place(x=270,y=600)
